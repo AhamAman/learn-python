574404b3668f5feca5b8ada75d6569c8c240a583
AhamAman <dasamankumar50@gmail.com>
2025-12-09 17:40:47 +0530

    ques-31

---
+++ set-union.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    english = set(map(int, input().strip().split(' ')))
+    m = int(input().strip())
+    french = set(map(int, input().strip().split(' ')))
+    
+    print(len(english.union(french)))
+    
