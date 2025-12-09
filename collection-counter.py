dfab703c5f6fa7ab70d3ff35d7fd0fa20b50e12a
AhamAman <dasamankumar50@gmail.com>
2025-12-09 20:15:21 +0530

    ques-51

---
+++ collection-counter.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from collections import Counter
+
+if __name__ == "__main__":
+    X = int(input().strip())
+    shoes = list(map(int, input().strip().split()))
+    N = int(input().strip())
+    result = 0
+    
+    warhs = Counter(shoes)
+    for _ in range(N):
+        size, money = map(int, input().strip().split(' '))
+        
+        if warhs[size] > 0:
+            result += money
+            warhs[size] -= 1
+        
+    print(result)
