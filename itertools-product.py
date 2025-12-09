05be89afe5956a2468ebccb050823f4ec89987a4
AhamAman <dasamankumar50@gmail.com>
2025-12-09 19:48:55 +0530

    ques-44

---
+++ itertools-product.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import product
+
+if __name__ == "__main__":
+    arr1 = list(map(int, input().strip().split(' ')))
+    arr2 = list(map(int, input().strip().split(' ')))
+    
+    for el in product(arr1, arr2):
+        print("{} ".format(el), end='')
