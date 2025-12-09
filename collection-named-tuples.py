7a729693e44d2f1289dc81d3a51f35860afef7c2
AhamAman <dasamankumar50@gmail.com>
2025-12-09 20:22:46 +0530

    ques-53

---
+++ collection-named-tuples.py
@@ -0,0 +1,20 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from collections import namedtuple
+
+if __name__ == "__main__":
+    s_num = int(input().strip())
+    tuple_fields = input().strip().split()
+    
+    student = namedtuple('student', tuple_fields)
+    library = []
+    res = 0
+    
+    for _ in range(s_num):
+        st_info = input().strip().split()
+        library.append(student(st_info[0], st_info[1], st_info[2], st_info[3]))
+        
+    for el in library:
+        res += int(el.MARKS)
+    
+    print(res/s_num)
